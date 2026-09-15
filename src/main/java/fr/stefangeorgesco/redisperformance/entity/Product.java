f43dc8297bfc61dfb59bb7538b0e3bc6a8c10770
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-15 14:47:27 +0200

    add redis

--- src/main/java/fr/stefangeorgesco/redisperformance/entity/Product.java
+++ src/main/java/fr/stefangeorgesco/redisperformance/entity/Product.java
@@ -1,7 +1,9 @@
 package fr.stefangeorgesco.redisperformance.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table
 public class Product {
 
     @Id
