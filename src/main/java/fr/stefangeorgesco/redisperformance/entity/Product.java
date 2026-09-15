fac58c2df9c5db1bc3366d8c1a789bacbd5de195
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-15 09:51:16 +0200

    add entity

---
+++ src/main/java/fr/stefangeorgesco/redisperformance/entity/Product.java
@@ -0,0 +1,44 @@
+package fr.stefangeorgesco.redisperformance.entity;
+
+import org.springframework.data.annotation.Id;
+
+public class Product {
+
+    @Id
+    private Integer id;
+    private String description;
+    private double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
