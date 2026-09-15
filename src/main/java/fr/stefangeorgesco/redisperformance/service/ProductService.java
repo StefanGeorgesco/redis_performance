ed976bc240dfcab30bc32aa3d58cfaeb67020df9
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-15 09:51:52 +0200

    add service

---
+++ src/main/java/fr/stefangeorgesco/redisperformance/service/ProductService.java
@@ -0,0 +1,26 @@
+package fr.stefangeorgesco.redisperformance.service;
+
+import fr.stefangeorgesco.redisperformance.entity.Product;
+import fr.stefangeorgesco.redisperformance.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<Product> getProduct(int id) {
+        return repository.findById(id);
+    }
+
+    public Mono<Product> updateProduct(int id, Mono<Product> productMono) {
+        return repository.findById(id)
+                .flatMap(p -> productMono.doOnNext(pr -> pr.setId(id)))
+                .flatMap(repository::save);
+    }
+}
